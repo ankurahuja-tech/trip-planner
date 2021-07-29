1bc8c577a507b119a08678ed8fc08084fe454669
Ankur Ahuja <AnkurAhuja.TECH@gmail.com>
2021-07-30 01:30:59 +0200

    add autocreate TripDays for Trips

--- trip_planner/trips/models.py
+++ trip_planner/trips/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.db import models
 
@@ -20,13 +22,47 @@ class Trip(TimeStampedModel):
     def __str__(self) -> str:
         return self.title
 
+    def create_trip_days(self):
+        """
+        Creates trip days for a new or updated Trip instance, provided that they are not already created.
+        """
+        trip_duration = self.end_date - self.start_date
+
+        # get list of dates for the Trip
+        trip_dates = []
+        for day in range(trip_duration.days + 1):
+            trip_date = self.start_date + datetime.timedelta(days=day)
+            trip_dates.append(trip_date)
+
+        # create TripDay instance for each day of the Trip
+        for trip_date in trip_dates:
+            TripDay.objects.get_or_create(trip=self, date=trip_date)
+
+    # def update_trip_days_nums(self): TODO
+    #     trip_days = TripDay.objects.all().filter(trip=self).order_by('-date') # "-" indicates descending order
+    #     num_increment = 1
+    #     for trip_day in trip_days:
+    #         trip_day.num = num_increment
+    #         num_increment += 1
+    #     TripDay.objects.bulk_update(trip_days, 'num')
+
+    def save(self, *args, **kwargs):
+        """
+        Save Trip object instance in the database and create TripDay objects for each day of the trip.
+        """
+        super().save(*args, **kwargs)
+        self.create_trip_days()
+        # self.update_trip_days_nums() TODO
+
 
 class TripDay(TimeStampedModel):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
-    date = models.DateField('date', help_text='This is a date within the trip.')  # add default values from Trip TODO
+    # num = models.IntegerField('day of the trip', help_text='This shows what day of the trip is this.', default=1) TODO
+    date = models.DateField('date', help_text='This is a date within the trip.')
     notes = models.TextField('Day notes', help_text='These are your notes for the day of the trip.', default='')
 
 
+# add Activity TODO
 # class Activity(TimeStampedModel):
 #     day = models.ForeignKey(TripDay, on_delete=models.CASCADE)
 #     title = models.CharField('Activity title', max_length=255, help_text='This is the title of your activity.')
--- trip_planner/trips/tests/test_models.py
+++ trip_planner/trips/tests/test_models.py
@@ -1,5 +1,8 @@
-import pytest
 import datetime
+
+import pytest
+
+# from unittest.mock import patch, MagicMock
 
 from config.settings.base import AUTH_USER_MODEL
 from trip_planner.trips.models import Trip, TripDay
@@ -32,7 +35,7 @@ def trip(user: AUTH_USER_MODEL) -> Trip:
 @pytest.fixture
 def trip_day(trip: Trip) -> TripDay:
     trip = trip
-    date = datetime.date(year=2021, month=7, day=30)
+    date = datetime.date(year=2021, month=1, day=1)
     trip_day = TripDay.objects.create(trip=trip, date=date)
     return trip_day
 
@@ -40,6 +43,9 @@ def trip_day(trip: Trip) -> TripDay:
 # ==============================================================================
 # TRIP TESTS
 # ==============================================================================
+
+
+# Generic Trip tests
 
 
 def test_trip_exists(trip: Trip) -> None:
@@ -58,16 +64,45 @@ def test_trip_str(trip: Trip) -> None:
     assert str(trip) == 'Test trip'
 
 
+# Create TripDays tests
+
+
+def test_create_trip_days_on_trip_creation(trip: Trip) -> None:
+    assert TripDay.objects.all().count() == 4
+
+
+def test_create_trip_days_on_trip_duration_extension(trip: Trip) -> None:
+    trip_duration_factor = datetime.timedelta(days=1)
+
+    trip.end_date += trip_duration_factor
+    trip.save(update_fields=['end_date'])
+    assert TripDay.objects.all().count() == 5
+
+    trip.start_date -= trip_duration_factor
+    trip.save(update_fields=['start_date'])
+    assert TripDay.objects.all().count() == 6
+
+
+def test_trip_day_num(trip: Trip) -> None:
+    trip_first_day = TripDay.objects.get(date=trip.start_date)
+    assert trip_first_day.num == 1
+
+
+# def test_change_trip_day_num_on_start_date_change(trip: Trip) -> None: TODO
+#     pass
+
+
 # ==============================================================================
 # TRIP DAY TESTS
 # ==============================================================================
 
 
-def test_trip_exists(trip: Trip) -> None:
-    assert Trip.objects.all().count() == 1
+def test_trip_day_exists(trip_day: TripDay) -> None:
+    expected_date = datetime.date(year=2021, month=1, day=1)
+    assert TripDay.objects.filter(date=expected_date).count() == 1
 
 
-def test_trip_fields(trip_day: TripDay) -> None:
+def test_trip_day_fields(trip_day: TripDay) -> None:
     assert trip_day.trip.title == 'Test trip'
-    assert str(trip_day.date) == "2021-07-30"
+    assert str(trip_day.date) == "2021-01-01"
     assert trip_day.notes == ''
